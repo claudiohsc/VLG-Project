ac6c1dbe14b1472aeac8e24ecf8bdc098d07ffbf
Claudio Henrique <claudio885henrique@gmail.com>
2022-03-16 10:57:05 -0300

    testes na saida de dados em csv

---
+++ functions/extrairdados.py
@@ -0,0 +1,26 @@
+import csv
+#Importa biblioteca para trabalhar com arquivos csv: tabelas para excel.
+
+fields = ['Name', 'CPF', 'Documentos', 'Etapa', 'Unidade']   
+#Determina as colunas da tabela.
+
+rows = [ ['Maria', '12345678900', '2', 'Gardenia', '17'],   
+         ['Jose', '12345678900', '2', 'Jacarandas', '58'],   
+         ['Paulo', '12345678900', '2', 'Pitangueiras', '23'],   
+         ['Carlos', '12345678900', '1', 'Orquideas', '35'],   
+         ['Ana', '12345678900', '1', 'Bougainville', '90'],   
+         ['Clara', '12345678900', '2', 'Azaleias', '13']]   
+#Determina as linhas da tabela em uma lista seguindo as colunas.
+
+filename = "dados.csv"
+#Declara o nome e extensão do arquivo de saída.
+
+with open(filename, 'w') as csvfile:   
+    
+    csvwriter = csv.writer(csvfile)   #Abre o arquivo e começa a escrever.
+          
+    
+    csvwriter.writerow(fields)   #colunas
+          
+    
+    csvwriter.writerows(rows) #linhas
